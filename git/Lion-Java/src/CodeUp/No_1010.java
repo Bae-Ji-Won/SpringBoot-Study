6184e952b8b2146dc2eec51517155981c714e782
qowl880 <qowhxk@naver.com>
2022-10-04 16:55:11 +0900

    [코드업] 1010

---
+++ git/Lion-Java/src/CodeUp/No_1010.java
@@ -0,0 +1,10 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class No_1010 {
+    public static void main(String[] args) {
+        int num = new Scanner(System.in).nextInt();
+        System.out.println(num);
+    }
+}
