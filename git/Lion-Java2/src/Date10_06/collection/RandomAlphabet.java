14d90fe135bab1603ac03dd643d274623f1bf4ff
JIWON <82360230+Bae-Ji-Won@users.noreply.github.com>
2022-10-27 14:18:54 +0900

    Delete git/Lion-Java2/src/Date10_06/collection directory

--- git/Lion-Java2/src/Date10_06/collection/RandomAlphabet.java
+++
@@ -1,9 +0,0 @@
-package Date10_06.collection;
-
-public class RandomAlphabet implements NumberGenerator{
-
-    @Override
-    public int generate(int num) {
-        return (int)(Math.random() * num);
-    }
-}
--- git/Lion-Java2/src/Date10_06/collection/RandomNumberGenerator.java
+++
@@ -1,9 +0,0 @@
-package Date10_06.collection;
-
-public class RandomNumberGenerator implements NumberGenerator{
-
-    @Override
-    public int generate(int num) {
-        return (int)(Math.random()*num);
-    }
-}
