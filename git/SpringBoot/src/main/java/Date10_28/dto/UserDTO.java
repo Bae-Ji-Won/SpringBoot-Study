94ea591fa3c742855fd6d95d109cfc2ba571e000
Bae-Ji-Won <qowhxk@naver.com>
2022-10-28 13:41:41 +0900

    DTO 추가

---
+++ git/SpringBoot/src/main/java/Date10_28/dto/UserDTO.java
@@ -0,0 +1,31 @@
+package Date10_28.dto;
+
+public class UserDTO {
+
+    private String id;
+    private String name;
+    private String password;
+
+    public UserDTO(String id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String toString(){
+        return "id =" +id + "name ="+ name + "password ="+password;
+    }
+}
