6af444c08e3ec86859abb081def6746e955702c2
Bae-Ji-Won <qowhxk@naver.com>
2022-10-31 14:22:10 +0900

    DTO-Hospital

---
+++ git/SpringBoot/src/main/java/Project/domain/Hospital.java
@@ -0,0 +1,38 @@
+package Project.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@AllArgsConstructor         // lombok이 알아서 생성자를 만들어줌 즉, 내가 만들지 않아도 됨
+@Getter                     // Getter 생성
+@NoArgsConstructor          // 빈 생성자 생성
+@Setter
+public class Hospital {
+    private int id;
+    private String openServiceName;
+    private int openLocalGovernmentCode;
+    private String managementNumber;
+    private LocalDateTime licenseDate;
+    private int businessStatus;
+    private int businessStatusCode;
+    private String phone;
+    private String fullAddress;
+    private String roadNameAddress;
+    private String hospitalName;
+    private String businessTypeName;
+    private int healthcareProviderCount;
+    private int patientRoomCount;
+    private int totalNumberOfBeds;
+    private float totalAreaSize;
+
+
+
+
+
+}
