424bd3f9d6c8c120bf49bf872b39a94491b31925
Bae-Ji-Won <qowhxk@naver.com>
2022-11-01 13:24:24 +0900

    DB연동하여 파싱하는 테스트 추가

--- git/SpringBoot/src/test/java/Project/parser/HospitalParserTest.java
+++ git/SpringBoot/src/test/java/Project/parser/HospitalParserTest.java
@@ -3,17 +3,33 @@ package Project.parser;
 import Project.domain.Hospital;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
+@SpringBootTest(classes = ParserFactory.class)
 class HospitalParserTest {
 
+    @Autowired      // new를 통한 객체 생성 대신 spring에서 자동으로 autowired를 통해 DI를 해줌
+    ReadLineContext<Hospital> hospitalReadLineContext;
+
+    @Test
+    void name() throws IOException {
+        // 서버환경에서 build할 때 문제가 생길 수 있다.
+        // 어디에서든 실행할 수 있게 자는 것이 목표
+        String filename = "C:\\SpringBoot_DBFile\\fulldata_01_01_02_P_의원2.csv";
+        List<Hospital> hospitalList = hospitalReadLineContext.readByLine(filename);
+        assertTrue(hospitalList.size() > 1000);
+        assertTrue(hospitalList.size() > 10000);
+    }
+
     String line1 = "\"1\",\"의원\",\"01_01_02_P\",\"3620000\",\"PHMA119993620020041100004\",\"19990612\",\"\",\"01\",\"영업/정상\",\"13\",\"영업중\",\"\",\"\",\"\",\"\",\"062-515-2875\",\"\",\"500881\",\"광주광역시 북구 풍향동 565번지 4호 3층\",\"광주광역시 북구 동문대로 24, 3층 (풍향동)\",\"61205\",\"효치과의원\",\"20211115113642\",\"U\",\"2021-11-17 02:40:00.0\",\"치과의원\",\"192630.735112\",\"185314.617632\",\"치과의원\",\"1\",\"0\",\"0\",\"52.29\",\"401\",\"치과\",\"\",\"\",\"\",\"0\",\"0\",\"\",\"\",\"0\",\"\",";
-    String line770 = "\"769\",\"의원\",\"01_01_02_P\",\"5710000\",\"PHMA119964360069041100011\",\"19960328\",\"\",\"01\",\"영업/정상\",\"13\",\"영업중\",\"\",\"\",\"\",\"\",\"298-8007\",\"\",\"360812\",\"충청북도 청주시 상당구 용암동 2627번지\",\"충청북도 청주시 상당구 중고개로 181, 2층 (용암동)\",\"28763\",\"상당이비인후과의원\",\"20170905183708\",\"I\",\"2018-08-31 23:59:59.0\",\"의원\",\"245973.624694\",\"346712.43852\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",\"\",";
 
     @Test
     @DisplayName("csv 1줄을 Hospital로 잘 만드는지 Test")
@@ -37,8 +53,6 @@ class HospitalParserTest {
         assertEquals(0, hospital.getPatientRoomCount()); //col:30
         assertEquals(0, hospital.getTotalNumberOfBeds()); //col:31
         assertEquals(52.29f, hospital.getTotalAreaSize()); //col:32
-
-
     }
 
 }
