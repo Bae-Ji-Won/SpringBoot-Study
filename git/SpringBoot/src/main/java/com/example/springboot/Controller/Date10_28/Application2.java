08bdaa454bbd2538c3153388502df818095b2549
Bae-Ji-Won <qowhxk@naver.com>
2022-11-01 14:29:31 +0900

    Application 파일

---
+++ git/SpringBoot/src/main/java/com/example/springboot/Controller/Date10_28/Application2.java
@@ -0,0 +1,13 @@
+package com.example.springboot.Controller.Date10_28;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application2 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application2.class, args);
+    }
+
+}
