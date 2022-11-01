4f2afe1199c9857cf7d3561b368ce45e2b6e409e
Bae-Ji-Won <qowhxk@naver.com>
2022-11-01 14:30:26 +0900

    TestCase - insert 추가

--- git/SpringBoot/src/test/java/Project/parser/HospitalParserTest.java
+++ git/SpringBoot/src/test/java/Project/parser/HospitalParserTest.java
@@ -1,6 +1,8 @@
 package Project.parser;
 
+import Project.dao.HospitalDao;
 import Project.domain.Hospital;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,23 +15,42 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-@SpringBootTest(classes = ParserFactory.class)
+@SpringBootTest
 class HospitalParserTest {
 
-    @Autowired      // new를 통한 객체 생성 대신 spring에서 자동으로 autowired를 통해 DI를 해줌
+    @Autowired      // @Configuration을 통해 설정한 Factory bean 파일 DI
     ReadLineContext<Hospital> hospitalReadLineContext;
 
+    @Autowired      // @Component을 통해 설정한 HospitalDao bean 파일 DI
+    HospitalDao hospitalDao;
+
+    String line1 = null;
+    @BeforeEach
+    void setup() {
+        line1 = "\"2\",\"의원\",\"01_01_02_P\",\"3620000\",\"PHMA119993620020041100014\",\"19990612\",\"\",\"01\",\"영업/정상\",\"13\",\"영업중\",\"\",\"\",\"\",\"\",\"062-515-2875\",\"\",\"500881\",\"광주광역시 북구 풍향동 565번지 4호 3층\",\"광주광역시 북구 동문대로 24, 3층 (풍향동)\",\"61205\",\"효치과의원\",\"20211115113642\",\"U\",\"2021-11-17 02:40:00.0\",\"치과의원\",\"192630.735112\",\"185314.617632\",\"치과의원\",\"1\",\"0\",\"0\",\"52.29\",\"401\",\"치과\",\"\",\"\",\"\",\"0\",\"0\",\"\",\"\",\"0\",\"\",";
+    }
+
     @Test
+    @DisplayName("insert가 잘 되었는지 확인")
+    void add(){
+        HospitalParser hp = new HospitalParser();
+        Hospital hospital = hp.parse(line1);
+        hospitalDao.add(hospital);
+    }
+    
+    @Test
+    @DisplayName("10만건 이상 파싱이 되었는지 확인")
     void name() throws IOException {
         // 서버환경에서 build할 때 문제가 생길 수 있다.
         // 어디에서든 실행할 수 있게 자는 것이 목표
         String filename = "C:\\SpringBoot_DBFile\\fulldata_01_01_02_P_의원2.csv";
         List<Hospital> hospitalList = hospitalReadLineContext.readByLine(filename);
+        
         assertTrue(hospitalList.size() > 1000);
         assertTrue(hospitalList.size() > 10000);
+
     }
 
-    String line1 = "\"1\",\"의원\",\"01_01_02_P\",\"3620000\",\"PHMA119993620020041100004\",\"19990612\",\"\",\"01\",\"영업/정상\",\"13\",\"영업중\",\"\",\"\",\"\",\"\",\"062-515-2875\",\"\",\"500881\",\"광주광역시 북구 풍향동 565번지 4호 3층\",\"광주광역시 북구 동문대로 24, 3층 (풍향동)\",\"61205\",\"효치과의원\",\"20211115113642\",\"U\",\"2021-11-17 02:40:00.0\",\"치과의원\",\"192630.735112\",\"185314.617632\",\"치과의원\",\"1\",\"0\",\"0\",\"52.29\",\"401\",\"치과\",\"\",\"\",\"\",\"0\",\"0\",\"\",\"\",\"0\",\"\",";
 
     @Test
     @DisplayName("csv 1줄을 Hospital로 잘 만드는지 Test")
